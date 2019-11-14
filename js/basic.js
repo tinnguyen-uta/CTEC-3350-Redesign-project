6a9c1420f0089de68afe687ce80f89af170ac701
tinnguyen-uta <57383385+tinnguyen-uta@users.noreply.github.com>
2019-11-14 13:15:09 -0600

    11/14

--- js/basic.js
+++ js/basic.js
@@ -1,33 +1,86 @@
 // this is an inline comment
+
 /*
  * Created by: Carlos Cucalon
  * Created on: 10/08/2019
  * Created for: CTEC 3350
- */
+*/
+
 // alert("hello world");
 // console.log("hello world");
-$(function() {
-	/*
-	 * Menu toggler
-	 * 1. When a user clicks on the toggler the navigation should open or close
-	 * 2. When a user clicks on the dropdown the dropdown menu should open or close
-	 */
-	let toggler = $(".navbar .navbar-toggler");
-	toggler.on("click", function() {
-		console.log("I was clicked");
-		let mainNavigation = $("#mainNavigation");
-		mainNavigation.fadeToggle(1);
-		let hidecontainer = $('main');
-		hidecontainer.fadeToggle(1);
-		let xbutton = $('.x-button');
-		xbutton.fadeToggle(1);
-		let burgerbutton = $('.hamburgers-button');
-		burgerbutton.fadeToggle(1);
-	});
-})
-$(function() {
-	let popup = $(".send-form");
-	popup.on("click", function() {
-		window.alert("This form is for display purposes, submitting this form will not send an email and will not be able to reach me. If you are interested in contacting me please send an email to: nguyenkimtin@gmail.com. Thank you");
-	});
-})
+
+$(function(){
+  /*
+  * Menu toggler
+  * 1. When a user clicks on the toggler the navigation should open or close
+  * 2. When a user clicks on the dropdown the dropdown menu should open or close
+  */
+
+  let toggler = $(".navbar .navbar-toggler");
+
+  toggler.on("click", function(){
+    console.log("I was clicked");
+    let mainNavigation = $("#mainNavigation");
+    mainNavigation.toggle(500);
+  });
+
+  let dropdown = $(".dropdown > .navbar-link");
+
+  dropdown.on("click", function(e){
+    console.log(e);
+    e.preventDefault();
+    console.log("I was clicked dropdown");
+    $(this).toggleClass("closed");
+    let dropdownMenu = $(this).parent().find(".dropdown-menu");
+    dropdownMenu.toggle();
+  });
+
+
+  let faqToggler = $(".faq-question");
+
+  faqToggler.on("click", function(){
+    $(this).toggleClass("closed");
+  })
+  
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*$(function(){
+
+  $(".navbar-toggler").on("click", function (){
+  	$(this).toggleClass("open");
+  	let menu = $(this).attr("data-target");
+  	$(menu).toggle(250);
+  });
+
+  $(".dropdown a").on("click", function (e){
+  	e.preventDefault();
+    $(this).parent().toggleClass("open");
+    $(this).parent().find(".dropdown-menu").toggle();
+  });
+
+});*/
