eb483f909b72f2be156b6f98f31e8bc0a79dc0d4
tinnguyen-uta <57383385+tinnguyen-uta@users.noreply.github.com>
2019-11-19 15:39:13 -0600

    updated home page

--- js/basic.js
+++ js/basic.js
@@ -54,7 +54,62 @@ $(function(){
   });
 });
 
+/*------------------rotate banner-----------------*/
 
+
+let heroControl = $(".hero-container .hero-control");
+let indicator = $(".hero-container .hero-indicators li");
+
+indicator.on("click", function(){
+	
+  let parentHeroContainer = $(this).closest(".hero-container");
+  let currentSlide = $(parentHeroContainer).find(".hero-indicators li.active").index();
+  let nextSlide = $(this).index();
+	
+  slideHandler(currentSlide, nextSlide, parentHeroContainer);
+  
+	
+});
+
+heroControl.on("click", function(){
+  console.log(this);
+  let direction = $(this).attr("data-slide");
+  let parentHeroContainer = $(this).closest(".hero-container");
+  let currentSlide = $(parentHeroContainer).find(".hero-slide.active").index();
+  let numberofSlides =$(parentHeroContainer).find(".hero-slide").length - 1;
+  let nextSlide;
+
+  if(direction === "next"){
+    nextSlide = currentSlide + 1;
+  } else if (direction === "prev"){
+    nextSlide = currentSlide - 1;
+  }
+	
+  if (nextSlide < 0){
+	  nextSlide = numberofSlides;
+  } else if (nextSlide > numberofSlides) {
+	  nextSlide = 0;
+  } else {
+	  nextSlide = nextSlide;
+  }
+	
+	
+  
+  
+	
+  console.log(direction, numberofSlides, nextSlide, currentSlide);
+	
+  slideHandler(currentSlide, nextSlide, parentHeroContainer);
+});
+
+function slideHandler (current, next, container){
+	console.log(current, next, container);
+	$(container).find(".hero-slide").eq(current).removeClass("active");
+	$(container).find(".hero-slide").eq(next).addClass("active");
+	$(container).find(".hero-indicators li").eq(current).removeClass("active");
+	$(container).find(".hero-indicators li").eq(next).addClass("active");
+	
+};
 
 
 
