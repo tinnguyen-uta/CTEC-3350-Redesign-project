4b5e0ea7745cd852d0b64c72ae20273c1fa08919
kathrynkerr <57072945+kathrynkerr@users.noreply.github.com>
2019-11-19 16:04:08 -0600

    Added requestinfo click 2 next Q

--- js/basic.js
+++ js/basic.js
@@ -36,8 +36,7 @@ $(function(){
   });
 
 
-  
-  
+
 
 });
 
@@ -61,14 +60,14 @@ let heroControl = $(".hero-container .hero-control");
 let indicator = $(".hero-container .hero-indicators li");
 
 indicator.on("click", function(){
-	
+
   let parentHeroContainer = $(this).closest(".hero-container");
   let currentSlide = $(parentHeroContainer).find(".hero-indicators li.active").index();
   let nextSlide = $(this).index();
-	
+
   slideHandler(currentSlide, nextSlide, parentHeroContainer);
-  
-	
+
+
 });
 
 heroControl.on("click", function(){
@@ -84,7 +83,7 @@ heroControl.on("click", function(){
   } else if (direction === "prev"){
     nextSlide = currentSlide - 1;
   }
-	
+
   if (nextSlide < 0){
 	  nextSlide = numberofSlides;
   } else if (nextSlide > numberofSlides) {
@@ -92,13 +91,13 @@ heroControl.on("click", function(){
   } else {
 	  nextSlide = nextSlide;
   }
-	
-	
-  
-  
-	
+
+
+
+
+
   console.log(direction, numberofSlides, nextSlide, currentSlide);
-	
+
   slideHandler(currentSlide, nextSlide, parentHeroContainer);
 });
 
@@ -108,13 +107,64 @@ function slideHandler (current, next, container){
 	$(container).find(".hero-slide").eq(next).addClass("active");
 	$(container).find(".hero-indicators li").eq(current).removeClass("active");
 	$(container).find(".hero-indicators li").eq(next).addClass("active");
-	
+
 };
 
 
 
 
+/** Request info functions**/
 
+/** click to next question**/
+
+let indicatorControl = $(".indicator-control") ;
+let indicator = $(".slide-indicators li") ;
+
+indicator.on("click", function(){
+  let parentContainer= $(this).closest(".request-container")
+  let currentSlide =$(parentContainer).find(".slide-indicators li.active").index();
+  let nextSlide = $(this).index();
+
+  slideHandler(currentSlide, nextSlide, parentContainer);
+});
+
+indicatorControl.on("click", function(){
+  console.log(this);
+  let direction = $(this).attr("data-slide");
+  let parentContainer= $(this).closest(".request-container")
+  let currentSlide =$(parentContainer).find(".page.active").index();
+  let numberOfSlides= $(parentContainer).find(".page").length -1;
+  let nextSlide;
+
+  if(direction == "next"){
+    nextSlide = currentSlide + 1;
+  } else if(direction == "prev"){
+    nextSlide = currentSlide -1;
+  }
+
+  if (nextSlide < 0){
+    nextSlide= numberOfSlides;
+  } else if (nextSlide > numberOfSlides) {
+    nextSlide = 0;
+  }else{
+    nextSlide = nextSlide;
+  }
+
+  console.log(direction, numberOfSlides, nextSlide, currentSlide);
+
+  slideHandler(currentSlide, nextSlide, parentContainer);
+});
+
+function slideHandler(current, next, container, indicators){
+  console.log(current, next);
+  $(container).find(".page").eq(current).removeClass("active");
+  $(container).find(".page").eq(next).addClass("active");
+
+  $(container).find(".slide-indicators li").eq(current).removeClass("active");
+  $(container).find(".slide-indicators li").eq(next).addClass("active");
+};
+
+/** end click to next question function **/
 
 
 
